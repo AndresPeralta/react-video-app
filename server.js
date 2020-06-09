c7e1f3aff09ef9fd842b2713dc612b5813f52316
andres <andres.n.peralta@gmail.com>
2020-06-09 10:21:02 -0300

    update serverjs file

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ const io = socket(server);
 const port = 8080;
 const users = {};
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 io.on('connection', socket => {
     if (!users[socket.id]) {
         users[socket.id] = socket.id;
