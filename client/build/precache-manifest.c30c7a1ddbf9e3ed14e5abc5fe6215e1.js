d088756188df5eddfc1ce947204ab83672221cdb
andres <andres.n.peralta@gmail.com>
2020-06-09 02:54:51 -0300

    removed unused script

---
+++ client/build/precache-manifest.c30c7a1ddbf9e3ed14e5abc5fe6215e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fcb3378b3ee3fc6230af21dc56bb8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f633c542a6c4adb9bb0",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ae06c70a78eff732004c",
+    "url": "/static/js/2.7311405c.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.7311405c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f633c542a6c4adb9bb0",
+    "url": "/static/js/main.8573b192.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
